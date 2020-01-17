66d147fbb4aff673a8150e461c4717b6394c551d
kojurama <39333419+kojurama@users.noreply.github.com>
2020-01-16 23:00:44 -0800

    Got json-server working for http calls

--- src/client/src/app/passenger-dashboard/passenger-dashboard.service.ts
+++ src/client/src/app/passenger-dashboard/passenger-dashboard.service.ts
@@ -1,18 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 import { Passenger } from './models/passenger.interface';
-
-const PASSENGER_API: string = '/api/passengers';
 
 @Injectable()
 export class PassengerDashboardService {
     constructor(private http: HttpClient) {}
+    url = 'http://localhost:4000';
 
     getPassengers(): Observable<Passenger[]> {
-        return this.http.get<Passenger[]>(PASSENGER_API);
+        return this.http.get<Passenger[]>(`${this.url}/passengers`);
     }
 }
